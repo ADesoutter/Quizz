02c80bff64195b2cc320210c8ea16b067941b38b
AlexD <alexandredesoutter.pro@gmail.com>
2021-06-20 17:27:57 +0200

    modif insert.sql

--- create.sql
+++ create.sql
@@ -4,32 +4,37 @@ CREATE DATABASE IF NOT EXISTS Quizz;
 -- Création d'une table "user"
     CREATE TABLE user ( 
  id INT UNSIGNED NOT NULL AUTO_INCREMENT ,
- `name` VARCHAR(50) NULL, 
- username VARCHAR(30) NOT NULL ,
- email VARCHAR(80) NOT NULL ,  
+ pseudo VARCHAR(30) NOT NULL ,
+ `password` VARCHAR(80) NOT NULL ,  
  PRIMARY KEY (id)) ENGINE = InnoDB;
 
 -- Création d'une table "catégories"
   CREATE TABLE categories ( 
  id INT UNSIGNED NOT NULL AUTO_INCREMENT ,
  title VARCHAR(100) NOT NULL, 
- body TEXT NULL , 
+ body TEXT NOT NULL , 
  user_id INT UNSIGNED NOT NULL,
  PRIMARY KEY (id)) ENGINE = InnoDB;
 
 -- Création d'une table "questions"
   CREATE TABLE questions ( 
  id INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+ -- title = "VRAI OU FAUX?"
  title VARCHAR(100) NOT NULL, 
- body TEXT NULL , 
- user_id INT UNSIGNED NOT NULL,
+ `description` TEXT NOT NULL , 
+ reponses_id INT UNSIGNED NOT NULL,
  PRIMARY KEY (id)) ENGINE = InnoDB;
 
 -- Création d'une table "réponses"
   CREATE TABLE reponses ( 
  id INT UNSIGNED NOT NULL AUTO_INCREMENT ,
- title VARCHAR(100) NOT NULL, 
+ title VARCHAR(255) NOT NULL, 
+ -- title = "la description de la question = TEXT"
+ `boolean` boolean NOT NULL,
+ `description` TEXT NOT NULL , 
+ questions_id INT UNSIGNED NOT NULL,
  body TEXT NULL , 
+
  PRIMARY KEY (id)) ENGINE = InnoDB;
  
  -- Création de clé étrangère
--- insert.sql
+++ insert.sql
@@ -1,3 +1,32 @@
 -- Insertion dans une table
-INSERT INTO post (title, body, user_id) VALUES 
-("Lorem ipsumogkogkgkg", "kdokdokosksdokfdoj ijdioj djsiji", 1);
+INSERT INTO questions (title, `description`, reponses_id) VALUES 
+("VRAI OU FAUX ?", "L'être humain dégage en chaleur une puissance 
+équivalente à celle de d'une ampoule électrique ?", 1);
+
+INSERT INTO reponses (title, `boolean`, `description`, questions_id) VALUES 
+("L'être humain dégage en chaleur une puissance équivalente 
+à celle d'une ampoule électrique", "Bravo! Bonne réponse. La réponse est vrai", 
+"En termes de chaleur, l'humain dégage une puissance de 80 à 100 watts 
+environ, ce qui équivaut à la puissance moyenne d'une ampoule électrique", 1);
+
+INSERT INTO questions (title, `description`, reponses_id) VALUES 
+("VRAI OU FAUX ?", "La première coupe du monde a eu lieu en 1930 ?", 2);
+
+INSERT INTO reponses (title, `boolean`, `description`, questions_id) VALUES 
+("La première coupe du monde a eu lieu en 1930", "Bravo! Bonne réponse. La réponse est VRAI", 
+"L'Uruguay a accueilli et gagné la première coupe du monde", 2);
+
+INSERT INTO questions (title, `description`, reponses_id) VALUES 
+("VRAI OU FAUX ?", "Il y eu des épreuves de foot à tous les JO d'été", 3);
+
+INSERT INTO reponses (title, `boolean`, `description`, questions_id) VALUES 
+("Il y a eu des épreuves de football à tous les JO d'été", "Désolé! Mauvaise réponse. La réponse est FAUX", 
+"Le football a fait des débuts officiels à la 4ème édition des JO d'été à Londres en 1908", 3);
+
+INSERT INTO questions (title, `description`, reponses_id) VALUES 
+("VRAI OU FAUX ?", "La FIFA a été fondée par la British Football Association en 1904", 4);
+
+INSERT INTO reponses (title, `boolean`, `description`, questions_id) VALUES 
+("La FIFA a été fondée par la British Football Association en 1904", 
+"Désolé! Mauvaise réponse. La réponse est FAUX", 
+"La FIFA a été fondée par les associations de football de la Belgique, du Danemark, de la France...", 4);
